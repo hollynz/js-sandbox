2cf8036deb4795a99621e62e2f1d4eae4eab73a2
Holly Johnson <hollyjnz@gmail.com>
2018-10-29 16:51:44 +1300

    added class clowns project

--- form-validate/js/main.js
+++ form-validate/js/main.js
@@ -7,14 +7,26 @@ let validUrl = /^(http[s]?:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,5}
 let inputEmailEl = document.querySelector('#inputEmail');
 let inputTelEl = document.querySelector('#inputTel');
 let inputUrlEl = document.querySelector('#inputUrl'); 
+let usernameEl = document.querySelector('#username');
 let emailErrorMessage = document.querySelector('#email-err-msg');
 let telErrorMessage = document.querySelector('#tel-err-msg');
 let urlErrorMessage = document.querySelector('#url-err-msg');
+let usernameErrorMessage = document.querySelector('#username-err-msg');
+
+// Misc
+let database = [
+    "hollyj",
+    "geoffs",
+    "asheighm",
+    "richardl",
+    "jeremys"
+];
 
 function validate(){
     let emailValidation = validateEmail(),
         telValidation = validateTelNo(),
-        urlValidation = validateUrl();
+        urlValidation = validateUrl(),
+        usernameValidation = validateUsername();
     if(!emailValidation){
         emailErrorMessage.style.display = "grid";
     }
@@ -31,7 +43,12 @@ function validate(){
     }else{
         urlErrorMessage.style.display = "none";
     }
-    return emailValidation && telValidation && urlValidation;
+    if(!usernameValidation){
+        usernameErrorMessage.style.display = "grid";
+    }else{
+        usernameErrorMessage.style.display = "none";
+    }
+    return emailValidation && telValidation && urlValidation && usernameValidation;
 }
 
 function validateEmail(){
@@ -53,6 +70,15 @@ function validateUrl(){
         return true;
     }
     return false;
+}
+
+function validateUsername(){
+    for(var i = 0; i < database.length; i++){
+        if(usernameEl.value === database[i]){
+            return false;
+        }
+    }
+    return true;
 }
 
 
@@ -81,5 +107,13 @@ inputUrlEl.addEventListener('blur', () => {
     }
     else {
         urlErrorMessage.style.display = "none";
+    }
+});
+
+usernameEl.addEventListener('blur', () => {
+    if(!validateUsername()){
+        usernameErrorMessage.style.display = "grid";
+    }else{
+        usernameErrorMessage.style.display = "none";
     }
 });
