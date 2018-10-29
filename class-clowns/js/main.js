2b34f0aea3bca632c61d4934dafac5519ebd23d8
Holly Johnson <hollyjnz@gmail.com>
2018-10-29 18:56:16 +1300

    started javascripting and styling

--- class-clowns/js/main.js
+++ class-clowns/js/main.js
@@ -1,22 +1,36 @@
-var studentCards = document.querySelector('#cardContainer');
-// Counter
-var timer = 0;
-var text = document.querySelector('#timingText');
+// Card array holding all image cards
+var cardArr = document.querySelectorAll('.card');
 
-var students = [
-  'ash',
-  'jeremy',
-  'richard',
-  'geoff',
-  'holly',
-  'lyndon',
-  'matthew'
-];
+// Card deck unordered list
+var cardsEl = document.querySelector('#cards');
+
+// Scoring variables
+var timer = 0, moves = 0;
+var counterEl = document.querySelector('.moves');
+
+// Matched cards
+var matchedCards = document.querySelectorAll('.match');
+
+var clickedCards = [];
+
+
+
+
+
+
 
 function randomise(){
 
 }
 
 function start(){
+
+}
+
+function showCard(){
   
+}
+
+for(var i = 0; i < cardArr.length; i++){
+  card = cardArr[i].addEventListener('click', showCard);
 }
